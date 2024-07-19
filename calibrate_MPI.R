fb321602605836a6233dd7d5aa90f9488f20ca01
alaiam <alaia.morell@gmail.com>
2024-07-19 01:42:14 -0700

    First commit of the calibration files, with a simple objective function (lnorm2 for biomass)

---
+++ calibrate_MPI.R
@@ -0,0 +1,94 @@
+rm(list=ls())
+
+library(calibrar)
+library(stringr)
+# #set config
+# usethis::use_git_config(user.name = "alaiam", user.email = "alaia.morell@gmail.com")
+# 
+# #Go to github page to generate token
+# usethis::create_github_token() 
+# 
+# #paste your PAT into pop-up that follows...
+# credentials::set_github_pat()
+
+require("atlantis2ls")
+require("calibrar")
+# devtools::install_github("alaiam/atlantis2ls")
+# Need to load the doSNOW package, which is installed in my HOME: /home1/datahome/nbarrier/libs/R/lib
+require("doSNOW")
+
+setwd("/home/atlantis/psatlantismodel/Calib_PSA")
+
+source("runModel_Atlantis.R")
+
+# creates a user defined likelihood function
+minmaxt = function(obs, sim) {
+  output = -1e+6*sum(log(pmin((sim+1)/(obs[, 1]+1), 1)), na.rm=TRUE) + 
+    1e+6*sum(log(pmax((sim+1)/(obs[, 2]+1), 1)), na.rm=TRUE)
+  return(output)
+}
+
+# reads calibration informations
+setup = calibration_setup(file="calibration_settings.csv") 
+observed = calibration_data(setup = setup,
+                           path=".", 
+                           file = NULL, 
+                           sep = ",")
+
+# load calibration parameters
+forcing = read.csv(file="calibration-parameters-complete.csv", 
+                     header=TRUE, 
+                     sep=",", 
+                     row.names=1)
+
+# create an objective function
+# additional arguments to the runModel function
+# are provided here.
+objfn = calibration_objFn(model=runModel, 
+                          setup=setup, 
+                          observed=observed, 
+                          aggregate=TRUE,
+                          forcing = forcing,
+                          names=row.names(forcing))
+
+control = list()
+control$maxit = c(10)   # maximum number of generations (former gen.max parameter)
+control$maxgen = c(10)   # maximum number of generations (former gen.max parameter)
+control$master = "/home/atlantis/psatlantismodel/Calib_PSA/master/"   # directory that will be copied
+control$run = "/home/atlantis/psatlantismodel/Calib_PSA/RUN"   # run directory
+control$restart.file = "/home/atlantis/psatlantismodel/Calib_PSA/restart_file"   # name of the restart file
+control$REPORT = 1    # number of generations to run before saving a restart
+control$parallel = TRUE
+control$nCores = 16
+control$popsize = 16   # population  size (former seed parameter)
+control$trace = 3 #global fitness and partial fitness
+
+# call the RMPI/Snow make cluster (note here that there are no arguments!)
+NumberOfCluster <- detectCores()  - 1
+cl <-  makeCluster(NumberOfCluster)
+
+# call the registerDoSNOW function instead of the registerDoParallel
+registerDoSNOW (cl)
+
+# send the variables and loaded libraries defined in the above to the nodes
+clusterExport(cl, c("control","objfn", "forcing", "observed", "setup", "minmaxt"))
+clusterEvalQ(cl, library("atlantis2ls"))
+clusterEvalQ(cl, library("calibrar"))
+clusterEvalQ(cl, library("stringr"))
+
+# run the calibration
+
+lbfgsb1 = calibrate(par=forcing['paropt'], fn=objfn, 
+                    method='Rvmmin', 
+                    lower=forcing['parmin'],
+                    upper=forcing['parmax'],
+                    phases=forcing['parphase'], 
+                    control=control, parallel = TRUE)
+
+# stop the cluster
+stopCluster(cl)
+
+
+# readRDS('restart_file.restart')
+readRDS('restart_file.results')
+
---
+++ runModel_Atlantis.R
@@ -0,0 +1,229 @@
+
+
+#' Sample runModel function for Atlantis. It writes the calibrated
+#' param (\code{param} argument) into a CSV file that will
+#' overwrite the atlantis forcing files.
+#' @param Parameter array.
+#' @param Parameter names
+#' @return A list containing all the variables
+#' used in the calibration
+runModel  = function(param, names, ...) {
+
+    # set parameter names
+    names(param) = names
+    write.table(param, file="calibration-parameters.csv", sep=",",
+                col.names=FALSE, quote=FALSE)
+    mum.factor = grep(x=names, pattern="mum")
+    BHalpha.factor = grep(x=names, pattern="BHalpha")
+    bio.prm = "AMPSbioparam_mv1_2022.prm"
+    bio.lines = readLines(bio.prm)
+
+    for (i in 1:length(mum.factor)){
+      species <- names[mum.factor[i]]
+      factor  <- param[mum.factor[i]]
+      species <- sub("mum_", "", species)
+      species <- sub("_factor", "", species)
+
+      bio.lines = edit_param_mum_sp(bio.lines, factor, species)
+    }
+
+    for (i in 1:length(BHalpha.factor)){
+      species <- names[BHalpha.factor[i]]
+      factor  <- param[BHalpha.factor[i]]
+      species <- sub("BHalpha_", "", species)
+      species <- sub("_factor", "", species)
+
+      bio.lines = edit_param_BHalpha_sp(bio.lines, factor, species)
+    }
+
+    writeLines(bio.lines, "bio.prm")
+
+    #TODO: Improve biomass function
+    #TODO: replace for loop
+
+
+
+##########################################################################################
+    # run Atlantis
+    sh.file = "amps_cal.sh"
+    run_atlantis(path = path, sh.file = sh.file)
+
+
+##########################################################################################
+    # read Atlantis outputs
+    path = "outputFolder"
+    prefix = "AMPS"
+    bio.prm = "AMPSbioparam_mv1_2022.prm"
+    fg.file <- "PugetSoundAtlantisFunctionalGroups_salmon_rectype4.csv"
+    outputs <- read_atlantis(path = path, prefix = prefix, fg.file = fg.file)
+
+    # extract the biomass, abundance, waa variables
+    atlantis.biomass = outputs$biomass
+    atlantis.abundance = outputs$abundance
+    atlantis.meanSizeByAge = outputs$waa
+      
+##########################################################################################
+    # Check if the configuration has run
+    # Find a way to return 0 if does not work
+
+
+##########################################################################################
+# Process the outputs so that calibrar like them
+    output = list(
+
+      # Biomass
+      MA_biomass = unname(unlist(atlantis.biomass["Macroalgae"])), 
+      ZS_biomass = unname(unlist(atlantis.biomass["Micro_Zoo"])), 
+      ZM_biomass = unname(unlist(atlantis.biomass["Meso_Zoo"])), 
+      ZL_biomass = unname(unlist(atlantis.biomass["Lrg_Zoo"])), 
+      ZG_biomass = unname(unlist(atlantis.biomass["Gel_Zoo"])), 
+      SQX_biomass = unname(unlist(atlantis.biomass["Squid"])), 
+      BMD_biomass = unname(unlist(atlantis.biomass["Macrobenth_Deep"])), 
+      BD_biomass = unname(unlist(atlantis.biomass["Deposit_Feeder"])), 
+      BG_biomass = unname(unlist(atlantis.biomass["Benthic_grazer"])), 
+      BMS_biomass = unname(unlist(atlantis.biomass["Octopi"])), 
+      DUN_biomass = unname(unlist(atlantis.biomass["Dungeness"])), 
+      BML_biomass = unname(unlist(atlantis.biomass["Crab"])), 
+      PWN_biomass = unname(unlist(atlantis.biomass["Shrimp"])), 
+      BFF_biomass = unname(unlist(atlantis.biomass["Filter_Other"])), 
+      BIV_biomass = unname(unlist(atlantis.biomass["Bivalve"])), 
+      GEC_biomass = unname(unlist(atlantis.biomass["Geoduck"])), 
+      BC_biomass = unname(unlist(atlantis.biomass["Carn_Infauna"])), 
+      HEP_biomass = unname(unlist(atlantis.biomass["Herring_PS"])), 
+      HEC_biomass = unname(unlist(atlantis.biomass["Herring_Cherry"])), 
+      FPS_biomass = unname(unlist(atlantis.biomass["Sm_Plank_Fish"])), 
+      POP_biomass = unname(unlist(atlantis.biomass["Perch_Fish"])), 
+      CHY_biomass = unname(unlist(atlantis.biomass["ChinookY_Fish"])), 
+      CHS_biomass = unname(unlist(atlantis.biomass["ChinookSY_Fish"])), 
+      CSY_biomass = unname(unlist(atlantis.biomass["ChinookSKY_Fish"])), 
+      CSS_biomass = unname(unlist(atlantis.biomass["ChinookSKSY_Fish"])), 
+      CSN_biomass = unname(unlist(atlantis.biomass["ChinookSSY_Fish"])), 
+      CDS_biomass = unname(unlist(atlantis.biomass["ChinookDSY_Fish"])), 
+      CNY_biomass = unname(unlist(atlantis.biomass["ChinookNY_Fish"])), 
+      CNS_biomass = unname(unlist(atlantis.biomass["ChinookNSY_Fish"])), 
+      CHC_biomass = unname(unlist(atlantis.biomass["ChinookHY_Fish"])), 
+      CYE_biomass = unname(unlist(atlantis.biomass["ChinookOY_Fish"])), 
+      CKS_biomass = unname(unlist(atlantis.biomass["ChinookOSY_Fish"])), 
+      COH_biomass = unname(unlist(atlantis.biomass["CohoHY_Fish"])), 
+      COS_biomass = unname(unlist(atlantis.biomass["CohoSY_Fish"])), 
+      COD_biomass = unname(unlist(atlantis.biomass["CohoDSY_Fish"])), 
+      COY_biomass = unname(unlist(atlantis.biomass["CohoOY_Fish"])), 
+      CMS_biomass = unname(unlist(atlantis.biomass["ChumHSY_Fish"])), 
+      CMF_biomass = unname(unlist(atlantis.biomass["ChumFSY_Fish"])), 
+      CMH_biomass = unname(unlist(atlantis.biomass["ChumHCSY_Fish"])), 
+      PIS_biomass = unname(unlist(atlantis.biomass["PinkSY_Fish"])), 
+      SAL_biomass = unname(unlist(atlantis.biomass["OtherSal_Fish"])), 
+      SAF_biomass = unname(unlist(atlantis.biomass["SGeorgia_Fish"])), 
+      FMM_biomass = unname(unlist(atlantis.biomass["Gadiods_Fish"])), 
+      FVS_biomass = unname(unlist(atlantis.biomass["LgDem_Fish"])), 
+      ROC_biomass = unname(unlist(atlantis.biomass["DemRock_Fish"])), 
+      MRO_biomass = unname(unlist(atlantis.biomass["MidRock_Fish"])), 
+      DVR_biomass = unname(unlist(atlantis.biomass["DemVRock_Fish"])), 
+      MVR_biomass = unname(unlist(atlantis.biomass["MidVRock_Fish"])), 
+      SMD_biomass = unname(unlist(atlantis.biomass["SmDem_Fish"])), 
+      FDF_biomass = unname(unlist(atlantis.biomass["SmFlat_Fish"])), 
+      HAP_biomass = unname(unlist(atlantis.biomass["PiscFlat_Fish"])), 
+      DOG_biomass = unname(unlist(atlantis.biomass["Spinydog_Fish"])), 
+      SBL_biomass = unname(unlist(atlantis.biomass["Sixgill_Shark"])), 
+      SSK_biomass = unname(unlist(atlantis.biomass["SkateRay"])), 
+      RAT_biomass = unname(unlist(atlantis.biomass["Ratfish"])), 
+      SB_biomass = unname(unlist(atlantis.biomass["Pisc_Seabird"])), 
+      SP_biomass = unname(unlist(atlantis.biomass["NonPisc_Seabird"])), 
+      BE_biomass = unname(unlist(atlantis.biomass["Raptors"])), 
+      HSL_biomass = unname(unlist(atlantis.biomass["Harbor_seal"])), 
+      CSL_biomass = unname(unlist(atlantis.biomass["California_sea_lion"])), 
+      PIN_biomass = unname(unlist(atlantis.biomass["Pinniped"])), 
+      PHR_biomass = unname(unlist(atlantis.biomass["Porpoise"])), 
+      ROR_biomass = unname(unlist(atlantis.biomass["Res_Orca"])), 
+      TOR_biomass = unname(unlist(atlantis.biomass["Trans_Orca"])), 
+      HUW_biomass = unname(unlist(atlantis.biomass["Hump_whale"])) 
+
+
+      # Thr
+      # Sm_Plank_Fish.Thr = atlantis.biomass["Sm_Plank_Fish"],
+      # Gadiods_Fish.Thr = atlantis.biomass["Gadiods_Fish"],
+      # Raptors.Thr = atlantis.biomass["Raptors"],
+      # Trans_Orcas.Thr = atlantis.biomass["Trans_Orcas"],
+      # Sm_Phyto.Thr = atlantis.biomass["Sm_Phyto"],
+      # Lrg_Phyto.Thr = atlantis.biomass["Lrg_Phyto"],
+      # Micro_Zoo.Thr = atlantis.biomass["Micro_Zoo"],
+      # Meso_Zoo.Thr = atlantis.biomass["Meso_Zoo"],
+      # Lrg_Zoo.Thr = atlantis.biomass["Lrg_Zoo"],
+      # Gel_Zoo.Thr = atlantis.biomass["Gel_Zoo"],
+      # Macroalgae.Thr = atlantis.biomass["Macroalgae"],
+      # Seagrass.Thr = atlantis.biomass["Seagrass"],
+      # Bivalve.Thr = atlantis.biomass["Bivalve"],
+      # Sed_Bact.Thr = atlantis.biomass["Sed_Bact"],
+      # Pelag_Bact.Thr = atlantis.biomass["Pelag_Bact"],
+
+
+      # Landings
+      # Sm_Plank_Fish.landings = atlantis.landings["Sm_Plank_Fish"],
+      # Gadiods_Fish.landings = atlantis.landings["Gadiods_Fish"],
+      # Meso_Zoo.landings = atlantis.landings["Meso_Zoo"],
+      # Lrg_Zoo.landings = atlantis.landings["Lrg_Zoo"],
+      # Macroalgae.landings = atlantis.landings["Macroalgae"],
+      # Seagrass.landings = atlantis.landings["Seagrass"],
+      # Bivalve.landings = atlantis.landings["Bivalve"],
+
+
+      # #Abundance
+      # Sm_Plank_Fish.abundance = atlantis.abundance["Sm_Plank_Fish"],
+      # Gadiods_Fish.abundance  = atlantis.abundance["Gadiods_Fish"],
+      # Raptors.abundance  = atlantis.abundance["Raptors"],
+      # Trans_Orcas.abundance = atlantis.abundance["Trans_Orcas"],
+
+
+      # Waa Sm_Plank_Fish
+      # Sm_Plank_Fish.meanSizeByAge1 = atlantis.meanSizeByAge["Sm_Plank_Fish1_StructN"],
+      # Sm_Plank_Fish.meanSizeByAge2 = atlantis.meanSizeByAge["Sm_Plank_Fish2_StructN"],
+      # Sm_Plank_Fish.meanSizeByAge3 = atlantis.meanSizeByAge["Sm_Plank_Fish3_StructN"],
+      # Sm_Plank_Fish.meanSizeByAge4 = atlantis.meanSizeByAge["Sm_Plank_Fish4_StructN"],
+      # Sm_Plank_Fish.meanSizeByAge5 = atlantis.meanSizeByAge["Sm_Plank_Fish5_StructN"],
+      # Sm_Plank_Fish.meanSizeByAge6 = atlantis.meanSizeByAge["Sm_Plank_Fish6_StructN"],
+      # Sm_Plank_Fish.meanSizeByAge7 = atlantis.meanSizeByAge["Sm_Plank_Fish7_StructN"],
+      # Sm_Plank_Fish.meanSizeByAge8 = atlantis.meanSizeByAge["Sm_Plank_Fish8_StructN"],
+      # 
+      # 
+      # 
+      # # Waa Gadiods_Fish
+      # Gadiods_Fish.meanSizeByAge1 = atlantis.meanSizeByAge["Gadiods_Fish1_StructN"],
+      # Gadiods_Fish.meanSizeByAge2 = atlantis.meanSizeByAge["Gadiods_Fish2_StructN"],
+      # Gadiods_Fish.meanSizeByAge3 = atlantis.meanSizeByAge["Gadiods_Fish3_StructN"],
+      # Gadiods_Fish.meanSizeByAge4 = atlantis.meanSizeByAge["Gadiods_Fish4_StructN"],
+      # Gadiods_Fish.meanSizeByAge5 = atlantis.meanSizeByAge["Gadiods_Fish5_StructN"],
+      # Gadiods_Fish.meanSizeByAge6 = atlantis.meanSizeByAge["Gadiods_Fish6_StructN"],
+      # Gadiods_Fish.meanSizeByAge7 = atlantis.meanSizeByAge["Gadiods_Fish7_StructN"],
+      # Gadiods_Fish.meanSizeByAge8 = atlantis.meanSizeByAge["Gadiods_Fish8_StructN"],
+      # Gadiods_Fish.meanSizeByAge9 = atlantis.meanSizeByAge["Gadiods_Fish9_StructN"],
+      # Gadiods_Fish.meanSizeByAge10 = atlantis.meanSizeByAge["Gadiods_Fish10_StructN"],
+      # 
+      # 
+      # # Waa Raptors
+      # Raptors.meanSizeByAge1 = atlantis.meanSizeByAge["Raptors1_StructN"],
+      # Raptors.meanSizeByAge2 = atlantis.meanSizeByAge["Raptors2_StructN"],
+      # Raptors.meanSizeByAge3 = atlantis.meanSizeByAge["Raptors3_StructN"],
+      # Raptors.meanSizeByAge4 = atlantis.meanSizeByAge["Raptors4_StructN"],
+      # Raptors.meanSizeByAge5 = atlantis.meanSizeByAge["Raptors5_StructN"],
+      # Raptors.meanSizeByAge6 = atlantis.meanSizeByAge["Raptors6_StructN"],
+      # Raptors.meanSizeByAge7 = atlantis.meanSizeByAge["Raptors7_StructN"],
+      # Raptors.meanSizeByAge8 = atlantis.meanSizeByAge["Raptors8_StructN"],
+      # Raptors.meanSizeByAge9 = atlantis.meanSizeByAge["Raptors9_StructN"],
+      # Raptors.meanSizeByAge10 = atlantis.meanSizeByAge["Raptors10_StructN"],
+      # 
+      # # Waa Trans orcas
+      # Trans_Orcas.meanSizeByAge1 = atlantis.meanSizeByAge["Trans_Orcas1_StructN"],
+      # Trans_Orcas.meanSizeByAge2 = atlantis.meanSizeByAge["Trans_Orcas2_StructN"],
+      # Trans_Orcas.meanSizeByAge3 = atlantis.meanSizeByAge["Trans_Orcas3_StructN"],
+      # Trans_Orcas.meanSizeByAge4 = atlantis.meanSizeByAge["Trans_Orcas4_StructN"],
+      # Trans_Orcas.meanSizeByAge5 = atlantis.meanSizeByAge["Trans_Orcas5_StructN"],
+      # Trans_Orcas.meanSizeByAge6 = atlantis.meanSizeByAge["Trans_Orcas6_StructN"],
+      # Trans_Orcas.meanSizeByAge7 = atlantis.meanSizeByAge["Trans_Orcas7_StructN"],
+      # Trans_Orcas.meanSizeByAge8 = atlantis.meanSizeByAge["Trans_Orcas8_StructN"],
+      # Trans_Orcas.meanSizeByAge9 = atlantis.meanSizeByAge["Trans_Orcas9_StructN"],
+      # Trans_Orcas.meanSizeByAge10 = atlantis.meanSizeByAge["Trans_Orcas10_StructN"]
+      )
+
+    return(output)
+
+}
